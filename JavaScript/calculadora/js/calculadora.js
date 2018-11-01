c11866012feaacacda7517081ceca26ac9186569
HeartBreaKiid <15171439@itculiacan.edu.mx>
2018-11-01 10:00:04 -0700

    Modificando JavaScript Calculadora

--- JavaScript/calculadora/js/calculadora.js
+++ JavaScript/calculadora/js/calculadora.js
@@ -1,6 +1,35 @@
-var coloramarillo = function(x){
-    x.style.background = "yellow";
+window.onload = function(){
+
+var coloramarillo = function(){
+    this.style.background = "yellow";
 }
-var colorblanco =function(x){
-    x.style.background = "white";
+var colorblanco =function(){
+    this.style.background = "white";
 }
+
+var operador1 = document.getElementById("operador1")
+operando1.addEventListener("focus", coloramarillo)
+operando1.addEventListener("focusout", colorblanco)
+
+}
+
+
+var operador="";
+var numeros=function(){
+    var valor=this.value;
+    if (operador==""){
+        var valorinput =document.getElementById("operando1").value;
+        if(valorinput=="0"){
+            document.getElementById("operando1").value="";
+        }
+        document.getElementById("operando1").value+=valor;
+    }else {
+        //operando2
+
+        document.getElementById("operando2").value="";
+        document.getElementById("operando2").value+=valor;
+    }
+}
+
+    var uno = document.getElementById("uno")
+    uno.addEventListener("click",numeros)
